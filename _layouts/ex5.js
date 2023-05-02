298bedec34438e0a00f46f41cafb47ce0eb02a8e
Emanuel Gabriel Motofelea <motofelea.io.emanuel@student.utcluj.ro>
2023-05-02 16:02:43 +0300

    cookie examples

---
+++ _layouts/ex5.js
@@ -0,0 +1,15 @@
+document.cookie = "reader=1; SameSite=None; Secure";
+
+function checkACookieExists() {
+  if (
+    document.cookie.split(";").some((item) => item.trim().startsWith("reader="))
+  ) {
+    const output = document.getElementById("a-cookie-existence");
+    output.textContent = '> The cookie "reader" exists';
+  }
+}
+
+function clearOutputACookieExists() {
+  const output = document.getElementById("a-cookie-existence");
+  output.textContent = "";
+}
---
+++ _layouts/script.js
@@ -0,0 +1,7 @@
+document.cookie = "session=test GDPR";
+document.cookie = "favorite_task=collect Data";
+function alertCookie() { 
+    alert(document.cookie);
+ }
+
+ 
