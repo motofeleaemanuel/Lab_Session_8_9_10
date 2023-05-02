0f99741b24b637a2ee738096b7d0926e5448d340
Emanuel Gabriel Motofelea <motofelea.io.emanuel@student.utcluj.ro>
2023-05-02 16:25:48 +0300

    cookie examples refactor3

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,3 +90,15 @@ function clearOutputACookieExists() {
   const output = document.getElementById("a-cookie-existence");
   output.textContent = "";
 }
+
+function checkCookieHasASpecificValue() {
+  if (document.cookie.split(";").some((item) => item.includes("reader=1"))) {
+    const output = document.getElementById("a-specific-value-of-the-cookie");
+    output.textContent = '> The cookie "reader" has a value of "1"';
+  }
+}
+
+function clearASpecificValueOfTheCookie() {
+  const output = document.getElementById("a-specific-value-of-the-cookie");
+  output.textContent = "";
+}
